bd63f28a634f0ba83c2279937dbef985b0e6becc
randomfactor <randumfaktor@hotmail.com>
2020-05-24 23:45:10 -0400

    animate a square and a cube

---
+++ MozTut/Pages/Ex03.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MozTut.Pages
+{
+    public class Ex03Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
